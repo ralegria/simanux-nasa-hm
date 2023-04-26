88e0b778d55518fdeeffcab6231809de0190ec98
Ricardo Alegria <ralegria1212@gmail.com>
2023-04-26 15:19:09 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./App.module.scss";
 
 function App() {
+  const [coordinates, setCoordinates] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setCoordinates(e.target.value);
+  };
+
+  const handleClick = () => {
+    setLoading(true);
+    fetchImage(coordinates);
+  };
+
+  const fetchImage = (coords) => {
+    const [latitude, longitude] = coords.split(",");
+    const apiKey = "9ixRXXctRst5hZPML6IR3zALdWgtvmUXNckEKHyk";
+    const url = `https://api.nasa.gov/planetary/earth/assets?lat=${latitude}&lon=${longitude}&date=2018-01-01&dim=0.15&api_key=${apiKey}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.url);
+        setLoading(false); // Set loading to false after image has been fetched
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          value={coordinates}
+          onChange={handleChange}
+          className={styles["input-field"]}
+          placeholder="Enter latitude, longitude (e.g. 40.7128,-74.006)"
+        />
+        <button onClick={handleClick} type="button" className={styles.button}>
+          Get Image
+        </button>
+      </div>
+      {loading ? (
+        <div className={styles.loading}>
+          <div className={styles.spinner}></div>
+        </div>
+      ) : (
+        imageUrl && (
+          <div className={styles.imageContainer}>
+            <img
+              src={imageUrl}
+              alt="Aerial photography"
+              className={styles.image}
+            />
+          </div>
+        )
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("renders the input and button", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(
+      /Enter latitude, longitude/i
+    );
+    const buttonElement = screen.getByRole("button", { name: /Get image/i });
+    expect(inputElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("fetches and displays the image on button click", async () => {
+    const mockData = { url: "https://mock-image-url.com/image.jpg" };
+    jest.spyOn(global, "fetch").mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(mockData),
+    });
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(
+      /Enter latitude, longitude/i
+    );
+    const buttonElement = screen.getByRole("button", { name: /Get image/i });
+    fireEvent.change(inputElement, { target: { value: "40.7128,-74.006" } });
+    fireEvent.click(buttonElement);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      "https://api.nasa.gov/planetary/earth/assets?lat=40.7128&lon=-74.006&date=2018-01-01&dim=0.15&api_key=9ixRXXctRst5hZPML6IR3zALdWgtvmUXNckEKHyk"
+    );
+    const imageElement = await screen.findByAltText(/Aerial photography/i);
+    expect(imageElement).toHaveAttribute("src", mockData.url);
+  });
 });
